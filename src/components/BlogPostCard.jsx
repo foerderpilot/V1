d5584b80b18c4b5da3a4ed487984bd1c67610310
Marius Lux <109797990+luxmarius@users.noreply.github.com>
2024-03-16 11:16:04 +0100

    Changes

--- src/components/BlogPostCard.jsx
+++ src/components/BlogPostCard.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-function BlogPostCard({ image, title, description }) {
+function BlogPostCard({ id, image, title, description }) {
     return (
         <div className="card w-auto glass text-left">
-            <figure><img src={image} alt="Blog Post"/></figure>
+            <figure><img src={image} alt="Blog Post" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{title}</h2>
                 <p>{description}</p>
                 <div className="card-actions justify-start">
-                    <button className="btn btn-primary">Lesen</button>
+                    <Link to={`/post/${id}`} className="btn btn-primary">Lesen</Link>
                 </div>
             </div>
         </div>
